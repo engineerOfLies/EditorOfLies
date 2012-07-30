85574a35a70c377500a7947e0c4d2ff7bd0bcac8
EngineerOfLies <EngineerOfLies@engineoflies.org>
2012-07-30 15:49:43 -0400

    Header loads from config

--- src/editor_header.c
+++ src/editor_header.c
@@ -1,8 +1,8 @@
 #include <eol_dialog.h>
 #include <eol_input.h>
-#include <eol_camera.h>
 #include <eol_logger.h>
 #include <eol_graphics.h>
+#include <eol_window.h>
 
 /*local global variabls*/
 
@@ -13,7 +13,24 @@
 
 void editor_header_update(eolWindow *win,GList *updates)
 {
+  GList *c;
+  eolComponent *comp = NULL;
+  if ((win == NULL)||(updates == NULL))return;
   
+  for (c = updates;c != NULL;c = c->next)
+  {
+    if (c->data == NULL)continue;
+    comp = (eolComponent *)c->data;
+    switch (comp->id)
+    {
+      case 0:
+        fprintf(stdout,"File pressed!\n");
+        break;
+      case 1:
+        fprintf(stdout,"Edit Keys pressed!\n");
+        break;
+    }
+  }
 }
 
 void editor_header_draw(eolWindow *win)
@@ -23,28 +40,9 @@ void editor_header_draw(eolWindow *win)
 void editor_header_window()
 {
   eolWindow *win;
-  eolComponent *comp;
   win = eol_window_load_from_file("ui/file_header.def");
   win->update = editor_header_update;
   win->draw = editor_header_draw;
-  fprintf(stdout,"loaded window: %s\n",win->name);
-  fprintf(stdout,"rect: %i,%i,%i,%i\n",
-          win->rect.x,
-          win->rect.y,
-          win->rect.w,
-          win->rect.h);
-  fprintf(stdout,"drawGeneric: %i\n",win->drawGeneric);
-  
-    comp = eol_button_stock_new(
-    3,
-    "test_button2",
-    eol_rectf(0.1,0.7,1,1),
-    win->rect,
-    "Test Entity",
-    0,
-    eolFalse
-  );
-  eol_window_add_component(win,comp);
 
 }
 
