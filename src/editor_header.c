d4d75061759bd2826b25a2ad134ceff5cffde4d7
EngineerOfLies <EngineerOfLies@engineoflies.org>
2012-07-30 00:16:20 -0400

    Setting up configs

--- src/editor_header.c
+++ src/editor_header.c
@@ -22,40 +22,30 @@ void editor_header_draw(eolWindow *win)
 
 void editor_header_window()
 {
-  eolWindow *win = eol_window_new();
-  eolComponent *comp = NULL;
-  eolUint w,h;
-  if (!win)
-  {
-    eol_logger_message(
-      EOL_LOG_ERROR,
-      "editor_header:failed to create base window!\n");
-    return;
-  }
-  eol_graphics_get_size(&w, &h);
-  strncpy(win->name,"editor_header",EOLLINELEN);
-  eol_rect_copy(&win->rect,eol_rect(0,0,w,64));
-  win->canHasFocus = eolTrue;
-  win->hasFocus = eolTrue;
-  win->drawGeneric = eolTrue;
-  win->componentCount = 0;
-  win->components = NULL;
-  win->customData = NULL;
-  win->custom_delete = NULL;
-  win->draw = NULL;
+  eolWindow *win;
+  eolComponent *comp;
+  win = eol_window_load_from_file("ui/file_header.def");
   win->update = editor_header_update;
   win->draw = editor_header_draw;
-  comp = eol_button_stock_new(
-    1,
-    "File",
-    eol_rectf(0.01,0.1,1,1),
+  fprintf(stdout,"loaded window: %s\n",win->name);
+  fprintf(stdout,"rect: %i,%i,%i,%i\n",
+          win->rect.x,
+          win->rect.y,
+          win->rect.w,
+          win->rect.h);
+  fprintf(stdout,"drawGeneric: %i\n",win->drawGeneric);
+  
+    comp = eol_button_stock_new(
+    3,
+    "test_button2",
+    eol_rectf(0.1,0.7,1,1),
     win->rect,
-    "File",
+    "Test Entity",
     0,
     eolFalse
   );
   eol_window_add_component(win,comp);
-  
+
 }
 
 
